7f278ce88d1947b8b0d242fb11e88ac795bd27bc
Steve Hughes <steve.hughes@creative-technologies.co.uk>
2020-02-10 19:39:30 +0000

    Initial commit. Empty console app. gitignore. editorconfig

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace PopTool
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
